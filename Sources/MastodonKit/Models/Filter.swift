86f47baf7dd0a4e4d2ea77800b97cd25ae3dd37f
Bruno Philipe <git@bruno.ph>
2020-06-14 01:16:04 +0200

    Add docs to Filter entity

--- Sources/MastodonKit/Models/Filter.swift
+++ Sources/MastodonKit/Models/Filter.swift
@@ -8,24 +8,35 @@
 
 import Foundation
 
+/// Represents a user-defined filter for determining which statuses should not be shown to the user.
 public class Filter: Codable {
 
+    /// The ID of the filter in the database.
     public let id: String
 
+    /// The text to be filtered.
     public let phrase: String
 
+    /// The contexts in which the filter should be applied.
     public let context: Context
 
+    /// When the filter should no longer be applied.
     public let expiresAt: Date?
 
+    /// Should matching entities in home and notifications be dropped by the server?
     public let irreversible: Bool
 
+    /// Should the filter consider word boundaries?
     public let wholeWord: Bool
 
     public enum Context: String, Codable {
+        /// home timeline
         case home
+        /// notifications timeline
         case notification
+        /// public timelines
         case `public`
+        /// expanded thread of a detailed status
         case thread
     }
 
